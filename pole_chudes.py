8615328fcc84181384f995645adc18e33ba85ff9
surov <surovfake@mail.ru>
2025-03-29 11:50:09 +0700

    doc for guess_letter()

--- pole_chudes.py
+++ pole_chudes.py
@@ -6,7 +6,11 @@ class PoleChudes:
         self.guessed_word = ["_" for _ in word]
 
     def guess_letter(self, letter):
-        #todo: реализовать метод guess_letter вместо заглушки
+        # todo: реализовать метод guess_letter вместо заглушки
+        # Метод, проверяющий наличие буквы в слове
+        # @param letter буква, введенная пользователем
+        # @return результат проверки слова (открыть отгаданную букву,
+        # либо оставить слово неизменным)
         if letter.upper() == "П":
             return ["П", "_", "_", "_", "_"]
         elif letter.upper() == "Н":
