affc3a7badfadaba0ef77497afd625bbdce94c61
surov <surovfake@mail.ru>
2025-03-29 20:22:15 +0700

    added test for PoleChudes.add_points

--- TESTS.py
+++ TESTS.py
@@ -15,3 +15,10 @@ def test_guess_word():
     game = PoleChudes("питон")
     assert game.guess_word("питон") is True
     assert game.guessed_word == ["П", "И", "Т", "О", "Н"]
+
+def test_points():
+    game = PoleChudes("питон")
+    game.add_points(100)
+    assert game.points == 100
+    game.add_points(50)
+    assert game.points == 150
