1bde11a2d4153fec376826da8be2a799d0b94956
surov <surovfake@mail.ru>
2025-03-29 11:33:27 +0700

    added guess_letter() trivial implementation

--- pole_chudes.py
+++ pole_chudes.py
@@ -5,3 +5,9 @@ class PoleChudes:
         self.word = word.upper()
         self.guessed_word = ["_" for _ in word]
 
+    def guess_letter(self, letter):
+        #todo: реализовать метод guess_letter вместо заглушки
+        if letter.upper() == "П":
+            return ["П", "_", "_", "_", "_"]
+        elif letter.upper() == "Н":
+            return ["П", "_", "_", "_", "Н"]
